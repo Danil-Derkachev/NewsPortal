7afc5c87e60739aa6b4d9fc54ec0da79eb80c954
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-10 19:00:03 +0300

    Добавил тесты

--- news_portal/my_news_portal/tests.py
+++ news_portal/my_news_portal/tests.py
@@ -4,32 +4,85 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from .models import *
+from .forms import *
 
 
 class TestMyNewsPortal(APITestCase):
     def setUp(self):
-        Category.objects.create(name='Спорт')
-        Category.objects.create(name='Наука')
-        User.objects.create(username='JohnDoe')
-        user_1 = User.objects.get(username='JohnDoe')
-        Author.objects.create(user=user_1)
+        self.category_1 = Category.objects.create(name='Спорт')
+        self.category_2 = Category.objects.create(name='Наука')
+        self.user_1 = User.objects.create(username='JohnDoe')
+        self.author_1 = Author.objects.create(user=self.user_1)
+        self.post_1 = Post.objects.create(author=self.author_1, type='NE', title='t', text='t')
+        self.comment_1 = Comment.objects.create(post=self.post_1, user=self.user_1, text='t')
 
-    def test_list_posts(self):
+    def test_list_posts_view(self):
         response = self.client.get(reverse('list_posts'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_news(self):
-        url = reverse('create_news')
-        response = self.client.get(url)
+    def test_detail_post_view(self):
+        response = self.client.get(reverse('detail_post', args=[self.post_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_form(self):
+        all_categories = Category.objects.all()
+        data = {'author': self.author_1, 'categories': all_categories, 'title': 'test_title', 'text': 'test_text'}
+        form = PostForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form(self):
+        data = {'text': 'random_text'}
+        form = CommentForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_create_news_view(self):
+        response = self.client.get(reverse('create_news'))
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
 
-        user_1 = User.objects.get(username='JohnDoe')
-        author_1 = Author.objects.get(user=user_1)
-        all_categories = Category.objects.all()
-        data = {'author': author_1, 'categories': all_categories, 'type': 'NE', 'title': 'test_title', 'text': 'test_text'}
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_create_article(self):
+    def test_create_article_view(self):
         response = self.client.get(reverse('create_article'))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_edit_post_view(self):
+        response = self.client.get(reverse('edit_post', args=[self.post_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_delete_post_view(self):
+        response = self.client.get(reverse('delete_post', args=[self.post_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_like_post(self):
+        response = self.client.post(reverse('like_post', args=[self.post_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_dislike_post(self):
+        response = self.client.get(reverse('dislike_post', args=[self.post_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_subscribe_to_category(self):
+        response = self.client.get(reverse('subscribe_to_category', args=[self.category_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_unsubscribe_from_category(self):
+        response = self.client.get(reverse('subscribe_to_category', args=[self.category_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_create_comment_view(self):
+        response = self.client.get(reverse('create_comment'))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_edit_comment_view(self):
+        response = self.client.get(reverse('edit_comment', args=[self.comment_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_delete_comment_view(self):
+        response = self.client.get(reverse('delete_comment', args=[self.comment_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_like_comment(self):
+        response = self.client.get(reverse('like_comment', args=[self.comment_1.id]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_dislike_comment(self):
+        response = self.client.get(reverse('dislike_comment', args=[self.comment_1.id]))
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
